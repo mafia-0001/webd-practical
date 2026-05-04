52a8513030ca8423a7be4186c5f13d448aace41d
mafia-0001 <sambhavs538@gmail.com>
2026-05-04 10:17:14 +0530

    Add files via upload

---
+++ Counter(q1).jsx
@@ -0,0 +1,51 @@
+import React, { useState } from "react";
+
+function App() {
+  
+  const [count, setCount] = useState(0);
+
+
+  const increment = () => {
+    setCount(count + 1);
+  };
+
+  const decrement = () => {
+    setCount(count - 1);
+  };
+
+  const reset = () => {
+    setCount(0);
+  };
+
+  return (
+    <div style={styles.container}>
+      <h1>Counter App</h1>
+      <h2>Count: {count}</h2>
+      <div>
+        <button onClick={increment} style={styles.button}>
+          Increment
+        </button>
+        <button onClick={decrement} style={styles.button}>
+          Decrement
+        </button>
+        <button onClick={reset} style={styles.button}>
+          Reset
+        </button>
+      </div>
+    </div>
+  );
+}
+const styles = {
+  container: {
+    textAlign: "center",
+    marginTop: "50px",
+  },
+  button: {
+    margin: "10px",
+    padding: "10px 20px",
+    fontSize: "16px",
+    cursor: "pointer",
+  },
+};
+
+export default App;
